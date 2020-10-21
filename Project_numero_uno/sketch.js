bb8fa2bec57bd631e2442e88bae8f9e8b137cfb2
ryank <64173139+ryankawahara@users.noreply.github.com>
2020-10-21 11:06:57 -0400

    Update sketch.js

--- Project_numero_uno/sketch.js
+++ Project_numero_uno/sketch.js
@@ -5,12 +5,15 @@ let rightClicker = false;
 let speed;
 let ylocStart=750;
 
+
 let box1;
 let box2;
 let move=200;
 allMove=200;
+let star1;
 
 function setup() {
+
   speed = 1;
   createCanvas(720, 720);
   background(116, 185, 255);
@@ -20,21 +23,26 @@ function setup() {
  
  box1 = new Box(1);
  box2 = new Box(2);
+ star1 = new Star(415,height);
 }
 
 
 function draw() {
  
   background(116, 185, 255);
-  push();
-  translate(100, 100);
+  
+//  push();
+ // translate(100, 100);
 
   //  rotate(radians(15));
-  rotate(frameCount / 200.0);
-    star();
-
+ // rotate(frameCount / 200.0);
   
-  pop();
+  
+//  pop();
+  star1.move();
+    star1.display();
+    
+
   rectangle();
       choose+=1;
     //  println(choose);
@@ -79,27 +87,7 @@ function draw() {
   
 }
 
-function star() {
-  fill(40, 154, 218);
-  noStroke();
-  circle(0, 0, 125);
-  stroke(255);
-  fill(255);
-  strokeWeight(2);
 
-  beginShape();
-  vertex(0, -50);
-  vertex(14, -20);
-  vertex(47, -15);
-  vertex(23, 7);
-  vertex(29, 40);
-  vertex(0, 25);
-  vertex(-29, 40);
-  vertex(-23, 7);
-  vertex(-47, -15);
-  vertex(-14, -20);
-  endShape(CLOSE);
-}
 
 function rectangle(){
   noStroke();
@@ -164,8 +152,8 @@ function mousePressed(){
 /*if ((box1.distanceX<42.5)&&(box1.distanceY<150))
 {
 
-	box1.yloc=ylocStart;
-	leftClicker = !leftClicker
+  box1.yloc=ylocStart;
+  leftClicker = !leftClicker
 
 }
 */
@@ -173,8 +161,8 @@ function mousePressed(){
 /*if ((box2.distanceX<42.5)&&(box2.distanceY<150))
 {
 
-	box2.yloc=ylocStart;
-	rightClicker = !rightClicker
+  box2.yloc=ylocStart;
+  rightClicker = !rightClicker
 
 }
 */
@@ -182,17 +170,17 @@ function mousePressed(){
 if ((box1.bounds()==true)){
 
 box1.yloc=ylocStart;
-	leftClicker = !leftClicker
-	box1.opacity=0;
-	box1.fadeOutOpacity=100;
+  leftClicker = !leftClicker
+  box1.opacity=0;
+  box1.fadeOutOpacity=100;
 
 }
 if ((box2.bounds()==true)){
 
-		box2.yloc=ylocStart;
-	rightClicker = !rightClicker
-	box2.opacity=0;
-	box2.fadeOutOpacity=100;
+    box2.yloc=ylocStart;
+  rightClicker = !rightClicker
+  box2.opacity=0;
+  box2.fadeOutOpacity=100;
 }
 
   
@@ -200,30 +188,34 @@ if ((box2.bounds()==true)){
 }
 
 class Box {
-	constructor(side){
-		this.side=side;
-		this.distanceX=0;
-		this.distanceY=0;
-		this.yloc=ylocStart;
-		this.opacity=0;
-		this.fadeOutOpacity=0;
-	}
+  constructor(side){
+    this.side=side;
+    this.distanceX=0;
+    this.distanceY=0;
+    this.yloc=ylocStart;
+    this.opacity=0;
+    this.fadeOutOpacity=0;
+    this.scale=1;
+  }
 speed(){
-	fill(0);
+  fill(0);
 if ((leftClicker == true)&&(this.side==1)){
       
         // rect(0,this.yloc-650,25,25);
         push();
+        scale(this.scale);
         translate(0,this.yloc-650,25,25);
        // rect(0,0,25,25);
-      	this.envelope();
+        this.envelope(1);
         pop();
      //   println("hello");
        this.yloc-=speed;
+       this.scale-=0.0025;
 
       
     }
-    if (this.yloc<623){ //625
+    if (this.yloc<600){ //625
+      this.scale=1;
       
         this.yloc=ylocStart; //720
 
@@ -231,16 +223,23 @@ if ((leftClicker == true)&&(this.side==1)){
     /*
     if (box2.yloc<623){
 
-    	box2.yloc=ylocStart;
+      box2.yloc=ylocStart;
 
     }
     */
 
   else if ((rightClicker==true)&&(this.side==2))
   {
-  	rect(0,this.yloc-650,25,25);
+    //rect(0,this.yloc-650,25,25);
      //   println("hello");
+      push();
+       scale(this.scale);
+        translate(0,this.yloc-650,25,25);
+       // rect(0,0,25,25);
+        this.envelope(2);
+        pop();
         this.yloc-=speed;
+         this.scale-=0.0025;
 
 
   }
@@ -249,7 +248,7 @@ if ((leftClicker == true)&&(this.side==1)){
 }
 
 launch(){
-	this.bounds();
+  this.bounds();
   stroke(0); 
   fill(255);
   rect(0,0,85,150); //white box
@@ -270,7 +269,7 @@ light(){
   if ((leftClicker == true)&&(this.side==1)){
 
   fill(76, 209, 55,this.opacity);
-	//print(opacity);
+  //print(opacity);
   ellipse(0,-55,20,20);
   this.opacity+=10;
 
@@ -301,7 +300,7 @@ light(){
   fill(76, 209, 55,this.fadeOutOpacity);
   ellipse(0,-55,20,20);
   this.fadeOutOpacity-=10;
-  print("yow");
+
 
 
 
@@ -321,30 +320,213 @@ if (mouseIsPressed==true){
 
 if ((this.distanceX<42.5)&&(this.distanceY<150))
 {
-	return true;
+  return true;
 
 }
 else{
 
-	return false;
+  return false;
 }
 }
 
 
 }
-envelope(){
-fill(255);
+envelope(whichSide){
+/*fill(255);
 rect(0,0,25,45);
 strokeWeight(0.5);
 fill(195,0,25);
 rect(0,10,10,10);
 fill(40,154,218);
 rect(0,-10,10,10);
+*/
+if (whichSide==1){ //if left side, top box is blue
+push();
+scale(0.75);
+ push();
+  fill(255);
+   // translate(width/2,height/2);
+  beginShape(QUADS);
+    vertex(20,-20);
+    vertex(-20,-20);
+//bottom half of white reectangle
+    vertex(-30,40);
+   vertex(30,40);
+  endShape();
+  push();
+  translate(0,-5);
+  scale(0.35);
+    beginShape(QUADS);
+    //blue checkbox
+  fill(40,154,218);
+    vertex(20,-20);
+    vertex(-20,-20);
+  //bottom half
+    vertex(-25,30);
+   vertex(25,30);
+  
+    
+  endShape();
+  pop();
+  
+    push();
+  translate(0,22);
+  scale(0.35);
+    beginShape(QUADS);
+    //bottom checkbox
+  fill(255);
+    vertex(20,-20);
+    vertex(-20,-20);
+  //bottom half
+    vertex(-25,30);
+   vertex(25,30);
+  
+    
+  endShape();
+  pop();
+  
+  
+  pop();
+  pop();
+
+}
+
+else if (whichSide==2){
+
+push();
+scale(0.75);
+ push();
+  fill(255);
+   // translate(width/2,height/2);
+  beginShape(QUADS);
+    vertex(20,-20);
+    vertex(-20,-20);
+//bottom half of white rectangle
+    vertex(-30,40);
+   vertex(30,40);
+  endShape();
+  push();
+  translate(0,-5);
+  scale(0.35);
+    beginShape(QUADS);
+    //white checkbox
+  fill(255);
+    vertex(20,-20);
+    vertex(-20,-20);
+  //bottom half
+    vertex(-25,30);
+   vertex(25,30);
+  
+    
+  endShape();
+  pop();
+  
+    push();
+  translate(0,22);
+  scale(0.35);
+    beginShape(QUADS);
+    //red checkbox
+fill(195,0,25);
+    vertex(20,-20);
+    vertex(-20,-20);
+  //bottom half
+    vertex(-25,30);
+   vertex(25,30);
+  
+    
+  endShape();
+  pop();
+  
+  
+  pop();
+  pop();
 
 
+
+
+}
 }
 
 
 }
+
+class Star {
+
+  constructor(x,y){
+
+this.pos = createVector(x,y);
+this.vel = createVector(0,-5);
+this.randomDirection;
+
+
+}
+
+display(){
+fill(40, 154, 218);
+  noStroke();
+  //background blue circle
+ 
+ 
+ //I got this code to draw a star from: 
+ //https://stackoverflow.com/questions/53799599/how-to-draw-a-star-shape-in-processingjs
+  push();
+ // rotate(frameCount / 200.0);
+  scale(0.65);
+  translate(this.pos.x,this.pos.y);
+   circle(0, 0, 125);
+  stroke(255);
+  fill(255);
+  strokeWeight(2);
+  rotate(frameCount / 100.0);
+  beginShape();
+  vertex(0, -50);
+  vertex(14, -20);
+  vertex(47, -15);
+  vertex(23, 7);
+  vertex(29, 40);
+  vertex(0, 25);
+  vertex(-29, 40);
+  vertex(-23, 7);
+  vertex(-47, -15);
+  vertex(-14, -20);
+  endShape(CLOSE);
+  pop();
+
+
+}
+
+move(){
+
 
   
+this.pos.add(this.vel);
+  this.randomDirection=p5.Vector.random2D();
+  this.randomDirection.mult(0.5);
+if ((this.pos.x > 1080)||(this.pos.x<0)){
+  this.vel.x=this.vel.x*-1;
+this.vel.y+=this.randomDirection.y;
+
+  
+}
+if ((this.pos.y>1080)||(this.pos.y<0)){
+  this.vel.y=this.vel.y*-1;
+    this.vel.x+=this.randomDirection.x;
+
+
+  print(this.pos.y);
+ // print('hi');
+}
+}
+
+//from Shiffman 1.2 video on Vectors
+
+
+
+}
+
+
+
+
+
+
+
